801e2b4cc4379c6ca35fb839a0b6970fdc2d9014
Suraj Yadav <2018surajyadav96@gmail.com>
2020-06-28 11:32:34 +0530

    Updates

---
+++ precache-manifest.23ed943e8eb0be5f3f4cc74a226d66bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601231acac8baee730b08c855fa69e02",
+    "url": "/react-crwn-clothing/index.html"
+  },
+  {
+    "revision": "1e6821f42376ba53b885",
+    "url": "/react-crwn-clothing/static/css/main.ce4f0438.chunk.css"
+  },
+  {
+    "revision": "bb9bdb8272cb5de05fca",
+    "url": "/react-crwn-clothing/static/js/2.8cac7982.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-crwn-clothing/static/js/2.8cac7982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6821f42376ba53b885",
+    "url": "/react-crwn-clothing/static/js/main.d8cefb5e.chunk.js"
+  },
+  {
+    "revision": "f0b1a046e045f98d04e0",
+    "url": "/react-crwn-clothing/static/js/runtime-main.3fd72630.js"
+  }
+]);
