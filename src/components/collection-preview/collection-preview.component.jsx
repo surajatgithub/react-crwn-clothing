e651bb8e40b874ce86ea7c9b47d92bf0be691b64
Suraj Yadav <2018surajyadav96@gmail.com>
2020-07-13 12:28:29 +0530

    master - Add collection preview and do cosmetic changes

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+
+import CollectionItem from "./../collection-item/collection-item.component";
+
 import "./collection-preview.component.scss";
 
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
-      <h1 className="title">{title.toLowerCase()}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
-          .map((item) => {
-            return <div key={item.id}>{item.name}</div>;
+          .map(({ id, ...itemOtherProps }) => {
+            return <CollectionItem key={id} {...itemOtherProps} />;
           })}
       </div>
     </div>
